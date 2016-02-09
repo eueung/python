3e51c1b1c254b386480928f9cbd026b80eac63af
EM <eueung@gmail.com>
2016-02-10 05:03:29 +0700

    add http codes

---
+++ http/server-01.py
@@ -0,0 +1,14 @@
+import SimpleHTTPServer
+import SocketServer
+# ---------------------------------------------------
+class GetHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        print self.headers
+        SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+# ---------------------------------------------------
+PORT    = 8000
+Handler = GetHandler
+httpd   = SocketServer.TCPServer(("", PORT), Handler)
+# ---------------------------------------------------
+print "serving at port", PORT
+httpd.serve_forever()
